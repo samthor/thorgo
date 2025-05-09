d11a242396b92ea2c0deef696885a4f208f9ecdb
Sam Thorogood <sam.thorogood@gmail.com>
2025-05-09 13:07:02 +1000

    stylistic

--- rope/js.go
+++ rope/js.go
@@ -6,9 +6,7 @@ import (
 
 // JSLength returns the JS length of the given string (which in Go, is always UTF-8).
 // We don't care about raw bytes because we probably only have this string because we got it _from_ JSON, which is UTF-8.
-func JSLength(s string) int {
-	var count int
-
+func JSLength(s string) (count int) {
 	for _, r := range s {
 		each := utf16.RuneLen(r)
 
