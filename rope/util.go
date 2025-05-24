3cfa57b3dd1d38e330e5c93da4483f22eb847725
Sam Thorogood <sam.thorogood@gmail.com>
2025-05-24 12:22:57 +1000

    math randomHeight

--- rope/util.go
+++ rope/util.go
@@ -5,8 +5,9 @@ import (
 	"math/rand/v2"
 )
 
-// randomHeight picks a height from 1-minHeight, inclusive.
+// randomHeight picks a height in the range [1,31], inclusive.
 // the odds of 2 is 50%, 3 is 25%, 4 is 12.5%... down to 32 at ~0.00000005%
 func randomHeight() int {
-	return min(maxHeight, 1+bits.LeadingZeros64(rand.Uint64()))
+	// always set bits 1+2, so this can at most return 30 plus our min 1 => [1,31].
+	return 1 + bits.TrailingZeros32(rand.Uint32()|3)
 }
