bc76b07d47acf2a0f290d1b7c307e29766f58a29
Sam Thorogood <sam.thorogood@gmail.com>
2025-07-20 14:36:52 +1000

    add fgn helper to cgroup

--- cgroup/cgroup.go
+++ cgroup/cgroup.go
@@ -11,8 +11,17 @@ type CGroup interface {
 	Add(c context.Context) (ok bool)
 
 	// Starts or retrieves the prior context for this CGroup.
-	// This will be already cancelled if no contexts were added.
+	// This will be already canceled if no contexts were added.
 	Start() context.Context
+
+	// Wait ensures this group has started, and then blocks until the [context.Context] is completed, returning its cause if not the default [context.Canceled].
+	Wait() error
+
+	// Go runs the given method as part of this group.
+	// It will only start after Start() has been called with valid contexts.
+	// Any returned error will cancel the group's [context.Context] directly, rather than with any provided cause.
+	// Returns true if the method has a chance of running.
+	Go(func(c context.Context) error) bool
 }
 
 // New creates a new CGroup, which simply provides a [context.Context] while any passed context is active.
@@ -33,6 +42,7 @@ type cgroup struct {
 	ctx    context.Context
 	cancel context.CancelCauseFunc
 	active int
+	tasks  []func(context.Context) error
 }
 
 func (cg *cgroup) Add(c context.Context) (ok bool) {
@@ -54,9 +64,12 @@ func (cg *cgroup) Add(c context.Context) (ok bool) {
 		cg.active--
 		if cg.active < 0 {
 			panic("got -ve active in CGroup")
+		} else if cg.active > 0 {
+			return
 		}
 
-		if cg.active == 0 && cg.cancel != nil {
+		// if all contexts run and die before Start(), we can still get a valid context if another one is added later :shrug:
+		if cg.ctx != nil {
 			cg.cancel(cg.cause)
 		}
 	})
@@ -76,12 +89,54 @@ func (cg *cgroup) Start() context.Context {
 	cg.lock.Lock()
 	defer cg.lock.Unlock()
 
-	if cg.ctx == nil {
-		cg.ctx, cg.cancel = context.WithCancelCause(context.Background())
-		if cg.active == 0 {
-			cg.cancel(cg.cause)
+	if cg.ctx != nil {
+		return cg.ctx
+	}
+
+	cg.ctx, cg.cancel = context.WithCancelCause(context.Background())
+	if cg.active == 0 {
+		cg.cancel(cg.cause)
+	} else {
+		for _, fn := range cg.tasks {
+			go cg.start(fn)
 		}
 	}
 
+	cg.tasks = nil
 	return cg.ctx
+}
+
+func (cg *cgroup) Wait() (err error) {
+	ctx := cg.Start()
+	<-ctx.Done()
+	err = context.Cause(ctx)
+	if err == context.Canceled {
+		err = nil
+	}
+	return
+}
+
+func (cg *cgroup) Go(fn func(context.Context) error) bool {
+	cg.lock.Lock()
+	defer cg.lock.Unlock()
+
+	if cg.ctx == nil {
+		cg.tasks = append(cg.tasks, fn)
+		return true
+	}
+
+	select {
+	case <-cg.ctx.Done():
+		return false
+	default:
+	}
+	go cg.start(fn)
+	return true
+}
+
+func (cg *cgroup) start(fn func(context.Context) error) {
+	err := fn(cg.ctx)
+	if err != nil {
+		cg.cancel(err)
+	}
 }
--- cgroup/cgroup_test.go
+++ cgroup/cgroup_test.go
@@ -3,6 +3,7 @@ package cgroup
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -51,5 +52,41 @@ func TestNoContext(t *testing.T) {
 
 	if context.Cause(cgCtx) != err {
 		t.Errorf("bad cause")
+	}
+}
+
+func TestGo(t *testing.T) {
+	cg := New()
+
+	var started bool
+	var run atomic.Int32
+
+	cg.Go(func(c context.Context) error {
+		if started == false {
+			t.Errorf("should not have been started")
+		}
+		run.Add(1)
+		return nil
+	})
+
+	time.Sleep(time.Millisecond)
+	if run.Load() != 0 {
+		t.Errorf("should not have run")
+	}
+
+	ctx, cancel := context.WithCancel(t.Context())
+	defer cancel()
+	cg.Add(ctx)
+
+	time.Sleep(time.Millisecond)
+	if run.Load() != 0 {
+		t.Errorf("should not have run")
+	}
+
+	started = true
+	cg.Start()
+	time.Sleep(time.Millisecond)
+	if run.Load() != 1 {
+		t.Errorf("should have run")
 	}
 }
